9554415b3f0a57646802cf0f4bf017b0e1985e2c
Rosario De Chiara <rosdec@gmail.com>
2022-12-17 20:16:15 +0100

    *** ADD *** First version with constants

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import BarChart from './BarChart'
+
+const App = () => {
+    return ( <BarChart /> )
+}
+
+export default App;
---
+++ src/BarChart.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3'
+
+
+class BarChart extends Component {
+    componentDidMount() {
+        this.drawChart();
+    }
+
+    drawChart() {
+        const data = [12, 5, 6, 6, 9, 10];
+
+
+        const svg = d3.select("body").append("svg").attr("width", 700).attr("height", 300);
+
+
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => i * 70)
+            .attr("y", (d, i) => 300 - 10 * d)
+            .attr("width", 65)
+            .attr("height", (d, i) => d * 10)
+            .attr("fill", "green");
+
+
+        svg.selectAll("text")
+            .data(data)
+            .enter()
+            .append("text")
+            .text((d) => d)
+            .attr("x", (d, i) => i * 70)
+            .attr("y", (d, i) => 300 - (10 * d) - 3)
+    }
+
+    render() {
+        return <div id={"#" + this.props.id}></div>
+    }
+}
+
+export default BarChart;
