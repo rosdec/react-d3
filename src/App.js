f90793939c389850380076d26c515c28893f29b8
Rosario De Chiara <rosdec@gmail.com>
2022-12-18 17:05:10 +0100

    ** UPD ** Updated the component definition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import BarChart from './BarChart'
 
 const App = () => {
-    return ( <BarChart /> )
+    return (<BarChart 
+        data={[12, 5, 6, 6, 9, 10]}
+        width={700}
+        height={300}/>)
 }
 
 export default App;
--- src/BarChart.js
+++ src/BarChart.js
@@ -8,11 +8,13 @@ class BarChart extends Component {
     }
 
     drawChart() {
-        const data = [12, 5, 6, 6, 9, 10];
+        console.log(this.props.data)
 
+        const data = this.props.data;
 
-        const svg = d3.select("body").append("svg").attr("width", 700).attr("height", 300);
-
+        const svg = d3.select("body").append("svg")
+            .attr("width", this.props.width)
+            .attr("height", this.props.height);
 
         svg.selectAll("rect")
             .data(data)
